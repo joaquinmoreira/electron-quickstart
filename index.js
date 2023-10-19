4142a06cd4133910aa76c77167cb1e9cf44d1731
Joaquín Moreira <joaquin@trytoku.com>
2023-10-19 14:12:04 -0300

    Add basic preload script

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require("electron");
+const { join } = require("path");
 
 app.whenReady().then(() => {
-  const window = new BrowserWindow();
+  const window = new BrowserWindow({
+    webPreferences: {
+      preload: join(__dirname, "./preload.js"),
+    },
+  });
   window.loadFile("index.html");
   window.webContents.openDevTools();
 });
---
+++ preload.js
@@ -0,0 +1,1 @@
+console.log(`Hello from preload with a node version:${process.versions.node}`);
